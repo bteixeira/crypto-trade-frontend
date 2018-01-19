c1867e8292639ea38859e1366c2e57c1834af653
Bruno Teixeira <bteixeira666@gmail.com>
2018-01-20 00:24:06 +0100

    Moved transactions table into separate view.

--- pub/js/app.js
+++ pub/js/app.js
@@ -4,8 +4,6 @@ $.ajaxSetup({
 	}
 })
 
-const $tbody = $('#table-transactions tbody')
-
 const balance = (function () {
 	var balance = new Map()
 
@@ -63,9 +61,11 @@ const balance = (function () {
 // })
 
 const transactions = new TransactionCollection()
-transactions.fetch({success: () => {
-	updateTransactions()
-}})
+new TransactionTableView({
+	el: '#table-transactions',
+	collection: transactions,
+})
+transactions.fetch()
 
 const filters = {
 	currency:'XRP',
@@ -87,41 +87,4 @@ function passFilter (transaction) {
 	}
 
 	return true
-}
-
-function updateTransactions () {
-	$tbody.empty()
-	balance.reset()
-	transactions.forEach(transaction => {
-		const traded = transaction.getTraded()
-		const payment = transaction.getPayment()
-		const fee = transaction.getFee()
-		const accountName = transaction.getAccount().getName()
-		const currencies = transaction.getCurrencies()
-		balance.add(traded, accountName)
-		if (traded.getAmount() < 0) {
-			balance.add(payment, accountName)
-		} else {
-			balance.add(payment.neg(), accountName)
-		}
-		balance.add(fee.neg(), accountName)
-		if (passFilter(transaction)) {
-			$tbody.append(`
-				<tr data-transaction-id="">
-					<td>${transaction.getTimestamp().toLocaleDateString()} ${transaction.getTimestamp().toLocaleTimeString()}</td>
-					<td>${accountName}</td>
-					<td>${transaction.format()}</td>
-					<td>${transaction.getPrice()}</td>
-					<td>${fee}</td>
-					<td>${transaction.getTotal()}</td>
-					<td>${balance.getAll(accountName).only(...currencies)}</td>
-					<td>${balance.getAll().only(...currencies)}</td>
-					<td>${transaction.get('blockchainRef')}</td>
-					<td>${transaction.get('observations')}</td>
-					<td><span class="action js-edit glyphicon glyphicon-pencil"/></td>
-					<td><span class="action js-delete glyphicon glyphicon-remove"/></td>
-				</tr>
-			`)
-		}
-	})
 }
--- pub/js/views/transaction-table.js
+++ pub/js/views/transaction-table.js
@@ -1,5 +1,43 @@
-const TransactionTable = Backbone.View.extend({
+const TransactionTableView = Backbone.View.extend({
+	initialize () {
+		this.listenTo(this.collection, 'sync change', this.render)
+		this.render()
+	},
 	render () {
-
+		const $tbody = this.$('tbody')
+		$tbody.empty()
+		balance.reset()
+		this.collection.forEach(transaction => {
+			const traded = transaction.getTraded()
+			const payment = transaction.getPayment()
+			const fee = transaction.getFee()
+			const accountName = transaction.getAccount().getName()
+			const currencies = transaction.getCurrencies()
+			balance.add(traded, accountName)
+			if (traded.getAmount() < 0) {
+				balance.add(payment, accountName)
+			} else {
+				balance.add(payment.neg(), accountName)
+			}
+			balance.add(fee.neg(), accountName)
+			if (passFilter(transaction)) {
+				$tbody.append(`
+				<tr data-transaction-id="">
+					<td>${transaction.getTimestamp().toLocaleDateString()} ${transaction.getTimestamp().toLocaleTimeString()}</td>
+					<td>${accountName}</td>
+					<td>${transaction.format()}</td>
+					<td>${transaction.getPrice()}</td>
+					<td>${fee}</td>
+					<td>${transaction.getTotal()}</td>
+					<td>${balance.getAll(accountName).only(...currencies)}</td>
+					<td>${balance.getAll().only(...currencies)}</td>
+					<td>${transaction.get('blockchainRef')}</td>
+					<td>${transaction.get('observations')}</td>
+					<td><span class="action js-edit glyphicon glyphicon-pencil"/></td>
+					<td><span class="action js-delete glyphicon glyphicon-remove"/></td>
+				</tr>
+			`)
+			}
+		})
 	}
 })
